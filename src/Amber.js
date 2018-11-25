66055546b28e1cf41fe2249d5449e626794266b4
nihakue <walden.gabrielw@gmail.com>
2018-11-25 16:14:07 +0000

    add glow and make text bigger

--- src/Amber.js
+++ src/Amber.js
@@ -3,11 +3,14 @@ import Stat from './Stat';
 import { PlayContext, changeAmber } from './Playmat';
 import './Amber.css';
 
+const KEY_COST = 6;
+
 export default function Amber() {
   const playState = useContext(PlayContext);
   const { amber, dispatch } = playState;
+  const ready = amber >= KEY_COST;
   return (
-    <section className="amber">
+    <section className={`amber${ready ? ' ready' : ''}`}>
       <button onClick={() => dispatch(changeAmber(-1))}>-</button>
       <Stat>{amber}</Stat>
       <button onClick={() => dispatch(changeAmber(1))}>+</button>

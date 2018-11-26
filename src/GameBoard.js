62267720adca99a432432ed87890998706c6f798
nihakue <walden.gabrielw@gmail.com>
2018-11-26 22:41:48 +0000

    Make button pressing clearer

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Playmat from './Playmat';
+import useFocusTrick from './useFocusTrick';
 import './GameBoard.css';
 
 export default function GameBoard() {
+  const userIsTabbing = useFocusTrick();
   return (
-    <div className="game-board">
+    <div className={`game-board${userIsTabbing ? ' tab-user' : ''}`}>
       <Playmat />
     </div>
   );
